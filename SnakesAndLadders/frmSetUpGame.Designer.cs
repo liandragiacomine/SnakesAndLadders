38b4c70b2a0aab929b6046cfd54df2f657356749
liandragiacomine <liandra11@hotmail.com>
2021-07-28 19:33:45 +0100

    UI design

---
+++ SnakesAndLadders/frmSetUpGame.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace SnakesAndLadders
+{
+	partial class frmSetPlayers
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnStart = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.lblID = new System.Windows.Forms.Label();
+			this.lbxPlayerID = new System.Windows.Forms.ListBox();
+			this.btnAddPlayer = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnStart
+			// 
+			this.btnStart.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.btnStart.Location = new System.Drawing.Point(284, 319);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(234, 42);
+			this.btnStart.TabIndex = 0;
+			this.btnStart.Text = "Start Game";
+			this.btnStart.UseVisualStyleBackColor = false;
+			//this.btnStart.Click += new System.EventHandler(this.btnStart.Click)
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(284, 72);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(234, 31);
+			this.textBox1.TabIndex = 1;
+			// 
+			// lblID
+			// 
+			this.lblID.AutoSize = true;
+			this.lblID.Location = new System.Drawing.Point(166, 75);
+			this.lblID.Name = "lblID";
+			this.lblID.Size = new System.Drawing.Size(86, 25);
+			this.lblID.TabIndex = 2;
+			this.lblID.Text = "Player ID:";
+			// 
+			// lbxPlayerID
+			// 
+			this.lbxPlayerID.BackColor = System.Drawing.Color.LightGray;
+			this.lbxPlayerID.FormattingEnabled = true;
+			this.lbxPlayerID.ItemHeight = 25;
+			this.lbxPlayerID.Location = new System.Drawing.Point(284, 136);
+			this.lbxPlayerID.Name = "lbxPlayerID";
+			this.lbxPlayerID.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.lbxPlayerID.Size = new System.Drawing.Size(234, 129);
+			this.lbxPlayerID.TabIndex = 3;
+			// 
+			// btnAddPlayer
+			// 
+			this.btnAddPlayer.Location = new System.Drawing.Point(550, 72);
+			this.btnAddPlayer.Name = "btnAddPlayer";
+			this.btnAddPlayer.Size = new System.Drawing.Size(86, 34);
+			this.btnAddPlayer.TabIndex = 4;
+			this.btnAddPlayer.Text = "Add";
+			this.btnAddPlayer.UseVisualStyleBackColor = true;
+			// 
+			// frmSetPlayers
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnAddPlayer);
+			this.Controls.Add(this.lbxPlayerID);
+			this.Controls.Add(this.lblID);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.btnStart);
+			this.Name = "frmSetPlayers";
+			this.ShowIcon = false;
+			this.Text = "Snakes And Ladders";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label lblID;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.ListBox lbxPlayerID;
+		private System.Windows.Forms.Button btnAddPlayer;
+	}
+}
+
---
+++ SnakesAndLadders/frmSnakesAndLadders.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace SnakesAndLadders
+{
+	partial class frmSnakesAndLadders
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSnakesAndLadders));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(28, 44);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Yellow;
+			this.button1.Location = new System.Drawing.Point(666, 361);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(143, 52);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Roll";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(666, 221);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(143, 115);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 2;
+			this.pictureBox2.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(678, 68);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(108, 25);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Player\'s turn";
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackColor = System.Drawing.Color.Red;
+			this.pictureBox3.Location = new System.Drawing.Point(38, 590);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(22, 22);
+			this.pictureBox3.TabIndex = 4;
+			this.pictureBox3.TabStop = false;
+			// 
+			// frmSnakesAndLadders
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(856, 680);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "frmSnakesAndLadders";
+			this.ShowIcon = false;
+			this.Text = "Snakes And Ladders";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox rtbBoard;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox3;
+	}
+}
